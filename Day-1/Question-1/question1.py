f0b74e42e4bb2597b250601b6d9d1a073eb26d91
Kyle <Kylenewton08@gmail.com>
2023-12-01 18:00:02 -0800

    completed question 1

--- Day-1/Question-1/question1.py
+++ Day-1/Question-1/question1.py
@@ -10,5 +10,37 @@ def strip_letters(s: str):
     return s2
 
 def read_from_txt(fileName: str):
+    lines = []
     with open(fileName) as openedFile:
-        lines = [line.rstrip() for line in openedFile]
+        for line in openedFile:
+            lines.append(line.rstrip())
+    return lines
+
+def read_and_strip_data():
+    data = read_from_txt("data.txt")
+    stripped_data = []
+    for d in data:
+        stripped_data.append(strip_letters(d))
+    return stripped_data
+
+def get_calibration_value(v):
+    first = v[0]
+    last = v[-1]
+    return str(first) + str(last)
+
+def get_calibration_values(values):
+    calibration_values = []
+    for v in values:
+        calibration_values.append(get_calibration_value(v))
+    return calibration_values
+
+def add_calibration_values(values):
+    total = 0
+    for v in values:
+        total = total + int(v)
+    return total
+
+data = read_and_strip_data()
+cali_values = get_calibration_values(data)
+total = add_calibration_values(cali_values)
+print(total)
